7367703e02d40935b9890352c02fb9241efc6bce
bridget <bridget@bridgets-Laptop.local>
2024-02-04 09:56:28 -0500

    update

--- api_lamda.py
+++ api_lamda.py
@@ -1,10 +1,11 @@
 import json
 
 def lambda_handler(event, context):
+    print("event:", event)
     body="hello from Lambda!"
     status_code = 200
     return {
-        "statuscode": status_code,
+        "statusCode": status_code,
         "body": json.dumps(body),
         "headers": {
             "content_type": "application/json"
